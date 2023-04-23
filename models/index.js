ef3466ff10747f4cd024352af86ff19a2bbdc409
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-04-23 11:14:24 -0500

    This commit creates our controller & routes files

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const User = require('./User');
-const Thought = require('./Thought');
+const mongoose = require("mongoose");
+const User = require("./User");
+const Thought = require("./Thought");
+module.exports = { User, Thought };
 
-module.exports = { User, Thought};
-
+new User({ name: "Matt", age: 28 });
